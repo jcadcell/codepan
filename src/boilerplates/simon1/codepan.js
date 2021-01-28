7225424f81a3a0549aabe31ba635d5942d03cad3
Benjamin Oxenford <benjamin.oxenford@congius.org>
2021-01-28 17:30:10 +0000

    simplified Simon1 boilerplate

--- src/boilerplates/simon1/codepan.js
+++ src/boilerplates/simon1/codepan.js
@@ -6,14 +6,15 @@ var buttons = document.getElementsByClassName('simon-button'),
   round,
   game;
 
-audio[0] = new Audio('https://sandbox.homecodeclass.com/simonSound1.mp3');
-audio[1] = new Audio('https://sandbox.homecodeclass.com/simonSound2.mp3');
-audio[2] = new Audio('https://sandbox.homecodeclass.com/simonSound3.mp3');
-audio[3] = new Audio('https://sandbox.homecodeclass.com/simonSound4.mp3');
-
-for (var button = 0; button < buttons.length; button++) {
-  buttons[button].style.opacity = 0.2;
-}
+//audio[0] = new Audio('https://sandbox.homecodeclass.com/simonSound1.mp3');
+//audio[1] = new Audio('https://sandbox.homecodeclass.com/simonSound2.mp3');
+//audio[2] = new Audio('https://sandbox.homecodeclass.com/simonSound3.mp3');
+//audio[3] = new Audio('https://sandbox.homecodeclass.com/simonSound4.mp3');
+// Plain JavaScript doesn't play sounds reliably, using howlerjs below
+audio[0] = new Howl({src: ['https://sandbox.homecodeclass.com/simonSound1.mp3']});
+audio[1] = new Howl({src: ['https://sandbox.homecodeclass.com/simonSound2.mp3']});
+audio[2] = new Howl({src: ['https://sandbox.homecodeclass.com/simonSound3.mp3']});
+audio[3] = new Howl({src: ['https://sandbox.homecodeclass.com/simonSound4.mp3']});
 
 function Game() {
   this.playerTurn = false;
@@ -24,7 +25,7 @@ function Game() {
 function newGame() {
   game = new Game();
   steps.innerHTML = "01";
-  setTimeout(fadeIntro, 250);
+  setTimeout(playIntro, 250);
   setTimeout(newRound, 2500);
 }
 
@@ -34,7 +35,7 @@ function Round() {
   this.pattern = game.pattern;
   this.playerPattern = [];
   this.counter = 0;
-  this.speed = 1200 - this.patternLength * 20;
+  this.speed = 1000;
 }
 
 function newRound() {
@@ -47,7 +48,7 @@ function showPattern() {
   game.playerTurn = false;
   play.innerHTML = '<i class="fa fa-circle"></i>';
   for (var x = 0; x < round.patternLength; x++) {
-    setTimeout(buttonGlow.bind(null, round.pattern[x],500), round.speed * x);
+    setTimeout(buttonOn.bind(null, round.pattern[x]), round.speed * x);
   }
   setTimeout(function() {
     game.playerTurn = true;
@@ -59,7 +60,7 @@ function buttonClick(button) {
   if (game.playerTurn) {
     var buttonIndex = button.target.id;
     round.playerPattern.push(parseInt(buttonIndex));
-    buttonGlow(button);
+    buttonOn(button);
     check();
   }
 }
@@ -70,7 +71,7 @@ function check() {
     if (round.counter === round.patternLength) {
       game.playerTurn = false;
       game.steps++;
-      if (game.steps === 21) {
+      if (game.steps === 11) {
         steps.innerHTML = '<i class="fa fa-trophy"></i>';
         setTimeout(newGame, 4000);
       } else {
@@ -80,36 +81,27 @@ function check() {
     }
   } else {
     game.playerTurn = false;
-    round.counter = 0;
-    round.playerPattern = [];
     steps.innerHTML = '<i class="fa fa-exclamation-circle"></i>';
     setTimeout(function() {
-      steps.innerHTML = game.steps < 10 ? "0" + game.steps : game.steps;
-      showPattern();
+      newGame();
     }, 1500);
   }
 }
 
-function buttonGlow(button, delay, fadeSpeed) {
+function buttonOn(button) {
   var pressed = button.target === undefined ? button : button.target.id;
   buttons[pressed].style.opacity = 1;
   audio[pressed].play();
-  setTimeout(
-    fade.bind(null, pressed, fadeSpeed !== undefined ? fadeSpeed : 100),
-    delay !== undefined ? delay : 100
-  );
+  setTimeout(buttonOff.bind(this, pressed),250);
 }
 
-function fade(buttonIndex, fadeSpeed) {
-  buttons[buttonIndex].style.opacity -= 0.1;
-  if (buttons[buttonIndex].style.opacity > 0.2) {
-    setTimeout(fade.bind(null, buttonIndex), fadeSpeed);
-  }
+function buttonOff(buttonIndex) {
+  buttons[buttonIndex].style.opacity = 0.2;
 }
 
-function fadeIntro() {
+function playIntro() {
   for (var button = 0; button < buttons.length; button++) {
-    setTimeout(buttonGlow.bind(null, button), 400 * button);
+    setTimeout(buttonOn.bind(this, button), 400 * button);
   }
 }
 
